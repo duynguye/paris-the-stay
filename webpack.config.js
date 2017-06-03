30baaf25a7afe8343904ba3d13652d7f74e974c2
Duy Nguyen <andynguyen5462@gmail.com>
2017-06-02 23:44:28 -0500

    Built production release.

--- webpack.config.js
+++ webpack.config.js
@@ -47,7 +47,9 @@ module.exports = {
     },
     plugins: [
         new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify('production')
+            'process.env': {
+                NODE_ENV: JSON.stringify('production')
+            }
         }),
         new webpack.optimize.UglifyJsPlugin({
             beautify: false,
